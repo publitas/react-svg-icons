2729bc491b962977103a079b632912346128d21a
Felix Hageloh <felix.hageloh@gmail.com>
2015-05-12 15:33:20 +0200

    should work completely without transforms now

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var React = require('react');
 var getSvgBody = require('./lib/get-svg-body');
-var inlineSvg = require('./inline-svg');
+var includeSvg = require('include-svg');
 var assign = require('react/lib/Object.assign');
 var resolveAssetPath = require('./lib/resolve-asset-path');
 
-var assets = inlineSvg(resolveAssetPath()); // asset path will be inlined
+var assets = includeSvg(resolveAssetPath());
 var icons = {};
 
 var Icon = React.createClass({
